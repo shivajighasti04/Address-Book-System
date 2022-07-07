0192e2077c42399fb370d9c320d90642fb0460b4
shivajighasti/04 <shivajighasti97@gmail.com>
2022-07-07 19:32:53 +0530

    Welcome Message

---
+++ AddressBookMain.java
@@ -0,0 +1,9 @@
+package com.bridgelabz.addressbook;
+
+public class AddressBookMain {
+
+	public static void main(String[] args) {
+		
+		 System.out.println("----------------Welcome To Address Book Management System !-------------------");
+	}
+}
