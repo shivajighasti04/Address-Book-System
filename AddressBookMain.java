b5e6403cedf465eaafcf432f85c1bbb49b0ebb06
shivajighasti/04 <shivajighasti97@gmail.com>
2022-07-30 19:46:54 +0530

    Program to sort the entries in the address book by City, State, or Zip

---
+++ AddressBookMain.java
@@ -0,0 +1,22 @@
+package com.bridgelabz.addressbook;
+
+import java.util.Scanner;
+
+/**
+ *Program to sort the entries in the address book by City, State, or Zip
+ * 
+ * @author Shivaji Ghasti
+ *
+ */
+public class AddressBookMain {
+	public static void main(String[] args) {
+		System.out.println("-----------------Welcome to address book  Program --------------------");
+		System.out.println();
+		
+		// Calling directoryMenu method to perform user choice of operation
+		    AddressBookDirectory addressBookDirectory = new AddressBookDirectory();
+		    addressBookDirectory.directoryMenu();
+	}
+}
+
+
